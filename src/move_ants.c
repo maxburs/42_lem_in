88c86bfd23751e31ad7d4a87d943e546a7ca321c
maxwell burson <maxwellburson@gmail.com>
2017-03-12 10:02:31 -0700

    woooorrrkinggggit status

--- src/move_ants.c
+++ src/move_ants.c
@@ -14,6 +14,7 @@
 #include <ft_printf.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <libft.h>
 
 static size_t	get_node_count(t_node *graph)
 {
@@ -59,7 +60,7 @@ int				*graph_order(t_node *graph)
 		i = 0;
 		while (order[i + 1] != -1)
 		{
-			if (graph[order[i]].distance < graph[order[i + 1]].distance)
+			if (graph[order[i]].distance > graph[order[i + 1]].distance)
 			{
 				swap = order[i];
 				order[i] = order[i + 1];
@@ -78,9 +79,76 @@ int				*graph_order(t_node *graph)
 	return (order);
 }
 
+static void		accept_ant(t_node *node)
+{
+	t_listm		*link;
+
+	if (node->ant)
+		return ;
+	link = node->links;
+	while (link)
+	{
+		if (((t_node*)(link->content))->ant
+			&& ((t_node*)(link->content))->distance > node->distance)
+		{
+			node->ant = ((t_node*)(link->content))->ant;
+			((t_node*)(link->content))->ant = 0;
+			ft_printf("L%d-%s ", node->ant, node->name);
+			return ;
+		}
+		link = link->next;
+	}
+}
+
+static void		accept_ants_to_end(t_node *node, int *ants_received)
+{
+	t_listm		*link;
+
+	link = node->links;
+	while (link)
+	{
+		if (((t_node*)(link->content))->ant)
+		{
+			ft_printf("L%d-%s ", ((t_node*)(link->content))->ant, node->name);
+			((t_node*)(link->content))->ant = 0;
+			*ants_received = *ants_received + 1;
+		}
+		link = link->next;
+	}
+}
+
 int				move_ants(t_node *graph, int ants)
 {
-	free(graph_order(graph));
-	UNUSED(ants);
+	int		*order;
+	int		ants_deployed;
+	int		ants_received;
+	t_node	*start;
+	t_node	*end;
+	size_t	i;
+
+	start = node_with_property(graph, "start");
+	end = node_with_property(graph, "end");
+	ants_received = 0;
+	ants_deployed = 1;
+	start->ant = ants_deployed;
+	if (!(order = graph_order(graph)))
+		return (1);
+	while (ants_received < ants)
+	{
+		i = 1;
+		accept_ants_to_end(end, &ants_received);
+		while (order[i] != -1)
+		{
+			accept_ant(graph + order[i]);
+			if (start->ant == 0 && ants_deployed < ants)
+			{
+				ants_deployed++;
+				start->ant = ants_deployed;
+			}
+			i++;
+		}
+		ft_putchar('\n');
+		//ft_printf("ants received: %d\n", ants_received);
+	}
 	return (0);
 }
